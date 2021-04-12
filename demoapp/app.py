2d2e43c4c5eae3314f5d411dae52d4dfd3d28824
NageshVagicharla <nagesh8421@gmail.com>
2021-04-12 16:49:56 -0400

    Added Jinja2 basics for home.html

--- demoapp/app.py
+++ demoapp/app.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Nagesh Vagicharla',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 12, 2021'
+    },
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 12, 2021'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page<h1>"
+    return render_template('about.html', title="About Page")
 
 if __name__ == '__main__':
     app.run(debug=True)
